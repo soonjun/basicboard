165092c29eb91506a6aa1ffcf91981ac8a362bd8
Sunjun Ok <remedime0@gmail.com>
2023-04-10 23:49:57 +0900

    Initial commit

---
+++ src/test/java/com/soonjun/basicboard/BasicboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soonjun.basicboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
